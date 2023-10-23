a72238acbf046d4b9ffee8ccddca4a5e70c289b2
Dan <peritract@hotmail.co.uk>
2023-10-23 16:26:20 +0100

    Create seed data

--- seed.sql
+++ seed.sql
@@ -14,3 +14,27 @@ VALUES
     ('Rhiannon', 'cerise', 205, 'F'),
     ('Tappon', 'crimson', 113, 'M')
 ;
+
+INSERT INTO mushroom_house
+    (house_number, street_name, max_occupants, roof_colour)
+VALUES
+    (12, 'Brookside', 4, 'red'),
+    (9, 'Brownie Boulevard', 1, 'green'),
+    (86, 'Elder Grove', 3, 'white'),
+    (31, 'Apple Lane', 2, 'red')
+;
+
+INSERT INTO mushroom_occupancy
+    (gnome_id, mushroom_house_id)
+VALUES
+    (1, 3),
+    (2, 3),
+    (3, 1),
+    (4, 1),
+    (5, 1),
+    (6, 1),
+    (7, 2),
+    (8, 2),
+    (9, 4),
+    (10, 4)
+;
